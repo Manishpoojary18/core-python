8e762c97886a30ce80a670fac7786ccd43eb9bbb
Manishpoojary18 <manishpoojary933@gmail.com>
2026-04-10 10:53:25 +0530

    add new programs

---
+++ 5-id,is.py
@@ -0,0 +1,15 @@
+# id() function - It is a predefined function that returns the id of the provided vlaue.add()
+# is operator - It checks whether both the values are in same memory location or not.
+a=5
+print(a)
+print(id(a))
+
+b=a
+print(b)
+print(id(b))
+print(a is b)
+
+c=a
+print(c)
+print(id(c))
+print(a is c)
