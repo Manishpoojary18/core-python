60e3e09c8816e225215aa596346dce215ee754b9
Manishpoojary18 <manishpoojary933@gmail.com>
2026-04-10 11:13:20 +0530

    adding new programs

--- 5-id,is.py
+++ 5-id,is.py
@@ -13,3 +13,21 @@ c=a
 print(c)
 print(id(c))
 print(a is c)
+
+
+# Integer interning - It is a type of optimization technique used by python to resue small integer objects instead of creating new ones repeatedly.
+# This saves memory and improves performance.
+# By default, python interns integer from -5 to 256
+
+d=20 
+e=20 
+f=20 
+print(d)
+print(e)
+print(f)
+print(id(d))
+print(id(e))
+print(id(f))
+print(d is e)
+print(d is a)
+print(d is b)
