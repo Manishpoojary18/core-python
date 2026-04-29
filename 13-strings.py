8602524b8404648252f9da21779e76cace6e9b7e
Manishpoojary18 <manishpoojary933@gmail.com>
2026-04-29 12:48:33 +0530

    arithmatic operations on strings

---
+++ 13-strings.py
@@ -0,0 +1,11 @@
+#Arithmatoic operations on strings 
+str1="Rama"
+str2="Sita"
+str3=str1+str2
+print(str3)#RamSita
+# str4=str1-str2
+# print(str4)#Error
+# str6=str1/str2
+# print(str6)#Error
+str7=str1*5
+print(str7)
